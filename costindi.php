236a0be8ecb78c2158e576a3043130dcf40250c6
Dave Ingram <dmi@teamgroupspaces.com>
2011-08-18 22:07:17 +0100

    The main costindi.php file is now executable and understands two common argument styles.
    These are the two styles supported by git:
     - oldfile newfile
     - path oldfile oldhex oldmode newfile newhex newmode

--- costindi.php
+++ costindi.php
@@ -1,11 +1,34 @@
+#!/usr/bin/php
 <?php
 
 require_once(__DIR__.'/src/TokenListDiff.php');
 require_once(__DIR__.'/src/Renderer/inline.php');
 
+array_shift($argv);
+if (count($argv) == 7) {
+	// called via GIT_EXTERNAL_DIFF
+	$oldfile = $argv[1];
+	$newfile = $argv[4];
+} elseif (count($argv) == 2) {
+	$oldfile = $argv[0];
+	$newfile = $argv[1];
+} else {
+	echo "Usage: costindi oldfile newfile\n";
+	exit(1);
+}
+
+if (!is_readable($oldfile)) {
+	echo "Cannot read file: $oldfile\n";
+	exit(1);
+}
+if (!is_readable($newfile)) {
+	echo "Cannot read file: $newfile\n";
+	exit(1);
+}
+
 $d = new TokenListDiff(
-	__DIR__.'/test_data/1a.php',
-	__DIR__.'/test_data/1b.php'
+	$oldfile,
+	$newfile
 );
 
 $r = new TokenListDiff_Renderer_inline();
