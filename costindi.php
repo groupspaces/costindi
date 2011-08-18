a8f8263377ee64aa75d9590588e0eb4d9a7c1c55
Mark Schaschke <mark@teamgroupspaces.com>
2011-08-18 21:45:33 +0100

    Update test wrapper to use the renderer

--- costindi.php
+++ costindi.php
@@ -1,8 +1,12 @@
 <?php
+
 require_once(__DIR__.'/src/TokenListDiff.php');
+require_once(__DIR__.'/src/Renderer/inline.php');
 
 $d = new TokenListDiff(
 	__DIR__.'/test_data/1a.php',
 	__DIR__.'/test_data/1b.php'
 );
-var_dump($d->getDiff());
+
+$r = new TokenListDiff_Renderer_inline();
+echo $r->render($d);
