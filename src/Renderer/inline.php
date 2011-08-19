4d5074e1dcdc6bdfe26255eafffbaaceaf151296
Mark Schaschke <mark@teamgroupspaces.com>
2011-08-19 01:40:04 +0100

    Import _context and _render from the main renderer, need to get into line context from token context

--- src/Renderer/inline.php
+++ src/Renderer/inline.php
@@ -20,7 +20,7 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 	/** @var  integer  Number of trailing context "lines" to preserve. */
 	public $_trailing_context_lines = 10000;
 
-	/** @var  array    Colours to use to highlight adds and deletes */
+	/** @var  array	Colours to use to highlight adds and deletes */
 	protected $_colours = array(
 		'ADD_FG' => null,
 		'ADD_BG' => array(0, 0x30, 0),  // green background
@@ -28,9 +28,9 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 		'DEL_BG' => array(0x30, 0, 0)   // red background
 	);
 
-	/** @var  array    Cache of generated console colours for adds and deletes */
+	/** @var  array	Cache of generated console colours for adds and deletes */
 	protected $colour = array('ADD' => null, 'DEL' => null);
-	/** @var  array    Cache of generated console reset codes */
+	/** @var  array	Cache of generated console reset codes */
 	protected $reset = null;
 
 	/**
@@ -63,6 +63,17 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 
 		return $this->reset;
 	}
+
+	/**
+	 * We don't indicate same lines, no need for indenting, so simply a wrapper around __lines()
+	 *
+	 * @see Text_Diff_Renderer::_context()
+	 */
+	public function _context($lines)
+	{
+		return $this->_lines($lines, '');
+	}
+
 
 	/**
 	 * Handle a copy diff
@@ -133,5 +144,85 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 	public function _changed($orig, $final)
 	{
 		return $this->_deleted($orig) . $this->_added($final);
+	}
+
+	/**
+	 * Renders a diff
+	 *
+	 * @param   Text_Diff  $diff  A Text_Diff object.
+	 * @return  string     The formatted output.
+	 */
+	function render($diff)
+	{
+		$xi = $yi = 1;
+		$block = false;
+		$context = array();
+
+		$nlead = $this->_leading_context_lines;
+		$ntrail = $this->_trailing_context_lines;
+
+		$output = $this->_startDiff();
+
+		$diffs = $diff->getDiff();
+		foreach ($diffs as $i => $edit) {
+			/* If these are unchanged (copied) lines, and we want to keep
+			 * leading or trailing context lines, extract them from the copy
+			 * block. */
+			if (is_a($edit, 'Text_Diff_Op_copy')) {
+				/* Do we have any diff blocks yet? */
+				if (is_array($block)) {
+					/* How many lines to keep as context from the copy
+					 * block. */
+					$keep = $i == count($diffs) - 1 ? $ntrail : $nlead + $ntrail;
+					if (count($edit->orig) <= $keep) {
+						/* We have less lines in the block than we want for
+						 * context => keep the whole block. */
+						$block[] = $edit;
+					} else {
+						if ($ntrail) {
+							/* Create a new block with as many lines as we need
+							 * for the trailing context. */
+							$context = array_slice($edit->orig, 0, $ntrail);
+							$block[] = new Text_Diff_Op_copy($context);
+						}
+						/* @todo */
+						$output .= $this->_block($x0, $ntrail + $xi - $x0,
+												 $y0, $ntrail + $yi - $y0,
+												 $block);
+						$block = false;
+					}
+				}
+				/* Keep the copy block as the context for the next block. */
+				$context = $edit->orig;
+			} else {
+				/* Don't we have any diff blocks yet? */
+				if (!is_array($block)) {
+					/* Extract context lines from the preceding copy block. */
+					$context = array_slice($context, count($context) - $nlead);
+					$x0 = $xi - count($context);
+					$y0 = $yi - count($context);
+					$block = array();
+					if ($context) {
+						$block[] = new Text_Diff_Op_copy($context);
+					}
+				}
+				$block[] = $edit;
+			}
+
+			if ($edit->orig) {
+				$xi += count($edit->orig);
+			}
+			if ($edit->final) {
+				$yi += count($edit->final);
+			}
+		}
+
+		if (is_array($block)) {
+			$output .= $this->_block($x0, $xi - $x0,
+									 $y0, $yi - $y0,
+									 $block);
+		}
+
+		return $output . $this->_endDiff();
 	}
 }
