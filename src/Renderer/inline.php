7805b571e99681491c3294dfc0f2da3f8a4b1d66
Mark Schaschke <mark@teamgroupspaces.com>
2011-08-19 01:25:45 +0100

    Add comments

--- src/Renderer/inline.php
+++ src/Renderer/inline.php
@@ -20,16 +20,26 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 	/** @var  integer  Number of trailing context "lines" to preserve. */
 	public $_trailing_context_lines = 10000;
 
+	/** @var  array    Colours to use to highlight adds and deletes */
 	protected $_colours = array(
-		'ADD_FG'    => null,
-		'ADD_BG'    => array(0, 0x30, 0),
+		'ADD_FG' => null,
+		'ADD_BG' => array(0, 0x30, 0),  // green background
 		'DEL_FG' => null,
-		'DEL_BG' => array(0x30, 0, 0)
+		'DEL_BG' => array(0x30, 0, 0)   // red background
 	);
 
+	/** @var  array    Cache of generated console colours for adds and deletes */
 	protected $colour = array('ADD' => null, 'DEL' => null);
+	/** @var  array    Cache of generated console reset codes */
 	protected $reset = null;
 
+	/**
+	 * Get the console colour for 'ADD' or 'DEL'
+	 * - caches if needed
+	 *
+	 * @param   string  $index  ADD or DEL
+	 * @return  Costindi\ConsoleColour
+	 */
 	protected function getColour($index)
 	{
 		if (!$this->colour[$index]) {
@@ -39,6 +49,12 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 		return $this->colour[$index];
 	}
 
+	/**
+	 * Get the console reset colour
+	 * - caches if needed
+	 *
+	 * @return  Costindi\ConsoleColour
+	 */
 	protected function getReset()
 	{
 		if (!$this->reset) {
@@ -48,6 +64,11 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 		return $this->reset;
 	}
 
+	/**
+	 * Handle a copy diff
+	 *
+	 * @see Text_Diff_Renderer::_lines()
+	 */
 	public function _lines($final, $prefix = ' ', $encode = true)
 	{
 		$res = '';
@@ -62,6 +83,13 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 		return $res;
 	}
 
+	/**
+	 * Handle an add or delete diff
+	 *
+	 * @param   array   $lines  Tokens to display
+	 * @param   string  $index  ADD or DEL
+	 * @return  string
+	 */
 	public function _processed($lines, $action)
 	{
 		$res = '';
@@ -80,16 +108,28 @@ class TokenListDiff_Renderer_inline extends Text_Diff_Renderer
 		return $res;
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see Text_Diff_Renderer::_added()
+	 */
 	public function _added($final)
 	{
 		return $this->_processed($final, 'ADD');
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see Text_Diff_Renderer::_deleted()
+	 */
 	public function _deleted($orig)
 	{
 		return $this->_processed($orig, 'DEL');
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see Text_Diff_Renderer::_changed()
+	 */
 	public function _changed($orig, $final)
 	{
 		return $this->_deleted($orig) . $this->_added($final);
